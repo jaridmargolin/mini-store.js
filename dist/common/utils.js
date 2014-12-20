7393f3e1391a3355c2ce1c05dff53fe9afc6ab72
Jarid Margolin <jaridmargolin@gmail.com>
2014-12-19 17:55:43 -0800

    v0.5.0

---
+++ dist/common/utils.js
@@ -0,0 +1,21 @@
+/*!
+ * utils.js
+ *
+ * Copyright (c) 2014
+ */
+
+
+var isObject = require('utl.js/dist/common/isObject');
+var jsonClone = require('assist.js/dist/common/jsonClone');
+
+
+/* -----------------------------------------------------------------------------
+ * utils
+ * ---------------------------------------------------------------------------*/
+
+module.exports = {
+  isObject: isObject,
+  jsonClone: jsonClone
+};
+
+
